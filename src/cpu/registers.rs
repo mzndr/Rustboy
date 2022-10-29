03f67848c98ba2cbeb12d59b5e0b282d74833928
Marius Zander <marius@bam-bam-bam.com>
2022-10-29 16:11:23 +0200

    wip

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -1,6 +1,10 @@
 use std::fmt;
 
 use crate::helpers;
+const FLAG_Z_MASK: u8 = 0b10000000;
+const FLAG_N_MASK: u8 = 0b01000000;
+const FLAG_H_MASK: u8 = 0b00100000;
+const FLAG_C_MASK: u8 = 0b00010000;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Registers {
@@ -82,67 +86,136 @@ impl Registers {
         self.c = split.1;
     }
 
+    // Gets the d rgister.
     pub fn get_d(&self) -> u8 {
         return self.d;
     }
+    // Sets the d rgister.
     pub fn set_d(&mut self, val: u8) {
         self.d = val;
     }
 
+    // Gets the e rgister.
     pub fn get_e(&self) -> u8 {
         return self.e;
     }
+    // Sets the e rgister.
     pub fn set_e(&mut self, val: u8) {
         self.e = val;
     }
 
+    // Gets the de rgister.
     pub fn get_de(&self) -> u16 {
         return helpers::merge_u8s(self.d, self.e);
     }
 
+    // Sets the de rgister.
     pub fn set_de(&mut self, val: u16) {
         let split = helpers::split_u16(val);
         self.d = split.0;
         self.e = split.1;
     }
 
+    /// Gets the h register.
     pub fn get_h(&self) -> u8 {
         return self.h;
     }
+    
+    /// Gets the h register.
     pub fn set_h(&mut self, val: u8) {
         self.h = val;
     }
 
+    /// Gets the l register.
     pub fn get_l(&self) -> u8 {
         return self.l;
     }
+    
+    /// Sets the l register.
     pub fn set_l(&mut self, val: u8) {
         self.l = val;
     }
 
+    /// Gets the hl register.
     pub fn get_hl(&self) -> u16 {
         return helpers::merge_u8s(self.h, self.l);
     }
 
+    /// Gets the sp register.
     pub fn set_hl(&mut self, val: u16) {
         let split = helpers::split_u16(val);
         self.h = split.0;
         self.l = split.1;
     }
 
+    /// Gets the sp register.
     pub fn get_sp(& self) -> u16 {
         return self.sp;
     }
+
+    /// Sets the sp register.
     pub fn set_sp(&mut self, val: u16) {
         self.sp = val;
     }
 
+    /// Gets the pc register.
     pub fn get_pc(&self) -> u16 {
         return self.pc;
     }
+
+    /// Sets the pc register.
     pub fn set_pc(&mut self, val: u16) {
         self.pc = val;
     }
+
+    /// FLAGS
+    pub fn get_flag_z(&self) -> u8 {
+        let mask: u8 = FLAG_Z_MASK;
+        return (self.f & mask) >> 7;
+    }
+
+    pub fn set_flag_z(&mut self, val: u8) {
+        self.f &= FLAG_Z_MASK;
+        self.f |= FLAG_Z_MASK;
+        self.f ^= FLAG_Z_MASK;
+        self.f |= val << 7;
+        
+    }
+
+    pub fn get_flag_n(&self) -> u8 {
+        return (self.f & FLAG_N_MASK) >> 6;
+    }
+
+    pub fn set_flag_n(&mut self, val: u8) {
+        self.f &= FLAG_N_MASK;
+        self.f |= FLAG_N_MASK;
+        self.f ^= FLAG_N_MASK;
+        self.f |= val << 6;
+    }
+
+    pub fn get_flag_h(&self) -> u8 {
+        return (self.f & FLAG_H_MASK) >> 5;
+    }
+
+    pub fn set_flag_h(&mut self, val: u8) {
+        self.f &= FLAG_H_MASK;
+        self.f |= FLAG_H_MASK;
+        self.f ^= FLAG_H_MASK;
+        self.f |= val << 5;
+    }
+
+    pub fn get_flag_c(&self) -> u8 {
+        return (self.f & FLAG_C_MASK) >> 4;
+    }
+
+    pub fn set_flag_c(&mut self, val: u8) {
+        self.f &= FLAG_C_MASK;
+        self.f |= FLAG_C_MASK;
+        self.f ^= FLAG_C_MASK;
+        self.f |= val << 4;
+    }
+
+
 }
 
 impl fmt::Display for Registers {
--- src/cpu/tests/registers.rs
+++ src/cpu/tests/registers.rs
@@ -1,6 +1,50 @@
 use crate::cpu::registers::Registers;
 
 #[test]
+fn get_register_logic() {
+    let mut r = Registers::new();
+    r.f = 0b10000000;
+    assert_eq!(r.get_flag_z(), 1);
+    r.f = 0b00000000;
+    assert_eq!(r.get_flag_z(), 0);
+}
+#[test]
+fn set_register_logic_0_1() {
+    let mut r = Registers::new();
+    r.f = 0b00000000;
+    r.set_flag_z(1);
+    let expected: u8 = 0b10000000;
+    assert_eq!(r.f, expected);
+}
+
+#[test]
+fn set_register_logic_1_1() {
+    let mut r = Registers::new();
+    r.f = 0b10000000;
+    r.set_flag_z(1);
+    let expected: u8 = 0b10000000;
+    assert_eq!(r.f, expected);
+}
+
+#[test]
+fn set_register_logic_1_0() {
+    let mut r = Registers::new();
+    r.f = 0b10000000;
+    r.set_flag_z(0);
+    let expected: u8 = 0;
+    assert_eq!(r.f, expected);
+}
+
+#[test]
+fn set_register_logic_0_0() {
+    let mut r = Registers::new();
+    r.f = 0b00000000;
+    r.set_flag_z(0);
+    let expected: u8 = 0;
+    assert_eq!(r.f, expected);
+}
+
+#[test]
 /// Test getting and setting of 
 /// all 16 bit registers.
 fn get_and_set_16bit_registers() {
