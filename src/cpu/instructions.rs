b960041e9fc3ba16d18b9fdf2a19391c0898f3d1
marius <zandermarius1@gmail.com>
2023-02-24 19:45:46 +0100

    add another few operations
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -13,8 +13,9 @@ impl Cpu {
             0x07 => self.rlca(),
             0x08 => self.ld_a16p_sp(),
             0x09 => self.add_hl_bc(),
-
+            0x0E => self.ld_c_d8(),
             0x13 => self.inc_de(),
+            0x19 => self.add_hl_de(),
             0x1D => self.dec_e(),
             0x20 => self.jr_nz_r8(),
             0x21 => self.ld_hl_d16(),
@@ -22,13 +23,15 @@ impl Cpu {
             0x23 => self.inc_hl(),
             0x25 => self.dec_h(),
             0x29 => self.add_hl_hl(),
+            0x2C => self.inc_l(),
             0x32 => self.ld_hlp_dec_a(),
             0x33 => self.inc_sp(),
             0x3E => self.ld_a_d8(),
+            0x4A => self.ld_c_d(),
+            0x4B => self.ld_c_e(),
             0xAF => self.xor_a(),
             0xC3 => self.jp_a16(),
             0xCB => self.exec_cb_instruction(),
-            0x0E => self.ld_c_d8(),
             
 
             _ => self.opcode_unknown(opcode),
@@ -138,6 +141,17 @@ impl Cpu {
         return 2;
     }
 
+    /// OPCode: 0x19
+    /// Mnemonic: ADD HL, DE
+    pub fn add_hl_de(&mut self) -> u8 {
+        let de = self.registers.get_de();
+        let hl = self.registers.get_hl();
+        let result = self.add16(hl, de);
+        self.registers.set_hl(result);
+        return 2;
+    }
+
+
     /// OPCode: 0x1D
     /// Mnemonic: DEC E
     pub fn dec_e(&mut self) -> u8 {
@@ -205,6 +219,14 @@ impl Cpu {
         return 2;
     }
 
+    /// OPCode: 0x2C
+    /// Mnemonic: INC L
+    pub fn inc_l(&mut self) -> u8 {
+        self.registers.l = self.inc8(self.registers.l);
+        return 1;
+    }
+
+
     /// OPCode: 0x32
     /// Mnemonic: INC Sd16P
     pub fn ld_hlp_dec_a(&mut self) -> u8 {
@@ -231,6 +253,229 @@ impl Cpu {
         return 2;
     }
 
+    /// OPCode: 0x40
+    /// Mnemonic: LD B, B
+    pub fn ld_b_b(&mut self) -> u8 {
+        self.registers.b = self.registers.b;
+        return 1;
+    }
+
+    /// OPCode: 0x41
+    /// Mnemonic: LD B, C
+    pub fn ld_b_c(&mut self) -> u8 {
+        self.registers.b = self.registers.c;
+        return 1;
+    }
+
+    /// OPCode: 0x42
+    /// Mnemonic: LD B, D
+    pub fn ld_b_d(&mut self) -> u8 {
+        self.registers.b = self.registers.d;
+        return 1;
+    }
+
+    /// OPCode: 0x43
+    /// Mnemonic: LD B, E
+    pub fn ld_b_e(&mut self) -> u8 {
+        self.registers.b = self.registers.e;
+        return 1;
+    }
+
+    /// OPCode: 0x44
+    /// Mnemonic: LD B, H
+    pub fn ld_b_h(&mut self) -> u8 {
+        self.registers.b = self.registers.h;
+        return 1;
+    }
+
+    /// OPCode: 0x45
+    /// Mnemonic: LD B, L
+    pub fn ld_b_l(&mut self) -> u8 {
+        self.registers.b = self.registers.l;
+        return 1;
+    }
+
+    /// OPCode: 0x46
+    /// Mnemonic: LD B, (HL)
+    pub fn ld_b_hlp(&mut self) -> u8 {
+        self.registers.b = self.read(self.registers.get_hl());
+        return 2;
+    }
+
+    /// OPCode: 0x47
+    /// Mnemonic: LD B, A
+    pub fn ld_b_a(&mut self) -> u8 {
+        self.registers.b = self.registers.a;
+        return 1;
+    }
+
+    /// OPCode: 0x48
+    /// Mnemonic: LD C, B
+    pub fn ld_c_b(&mut self) -> u8 {
+        self.registers.c = self.registers.b;
+        return 1;
+    }
+
+    /// OPCode: 0x49
+    /// Mnemonic: LD C, C
+    pub fn ld_c_c(&mut self) -> u8 {
+        self.registers.c = self.registers.c;
+        return 1;
+    }
+
+    /// OPCode: 0x4A
+    /// Mnemonic: LD C, D
+    pub fn ld_c_d(&mut self) -> u8 {
+        self.registers.c = self.registers.d;
+        return 1;
+    }
+
+    /// OPCode: 0x4B
+    /// Mnemonic: LD C, E
+    pub fn ld_c_e(&mut self) -> u8 {
+        self.registers.c = self.registers.e;
+        return 1;
+    }
+
+    /// OPCode: 0x4C
+    /// Mnemonic: LD C, H
+    pub fn ld_c_h(&mut self) -> u8 {
+        self.registers.c = self.registers.h;
+        return 1;
+    }
+
+    /// OPCode: 0x4D
+    /// Mnemonic: LD C, L
+    pub fn ld_c_l(&mut self) -> u8 {
+        self.registers.c = self.registers.l;
+        return 1;
+    }
+
+    /// OPCode: 0x4E
+    /// Mnemonic: LD C, (HL)
+    pub fn ld_c_hlp(&mut self) -> u8 {
+        self.registers.c = self.read(self.registers.get_hl());
+        return 2;
+    }
+
+    /// OPCode: 0x4F
+    /// Mnemonic: LD C, A
+    pub fn ld_c_a(&mut self) -> u8 {
+        self.registers.c = self.registers.a;
+        return 1;
+    }
+
+    /// OPCode: 0x50
+    /// Mnemonic: LD D, B
+    pub fn ld_d_b(&mut self) -> u8 {
+        self.registers.b = self.registers.b;
+        return 1;
+    }
+
+    /// OPCode: 0x51
+    /// Mnemonic: LD D, C
+    pub fn ld_d_c(&mut self) -> u8 {
+        self.registers.b = self.registers.c;
+        return 1;
+    }
+
+    /// OPCode: 0x52
+    /// Mnemonic: LD D, D
+    pub fn ld_d_d(&mut self) -> u8 {
+        self.registers.b = self.registers.d;
+        return 1;
+    }
+
+    /// OPCode: 0x53
+    /// Mnemonic: LD D, E
+    pub fn ld_d_e(&mut self) -> u8 {
+        self.registers.b = self.registers.e;
+        return 1;
+    }
+
+    /// OPCode: 0x54
+    /// Mnemonic: LD D, H
+    pub fn ld_d_h(&mut self) -> u8 {
+        self.registers.b = self.registers.h;
+        return 1;
+    }
+
+    /// OPCode: 0x55
+    /// Mnemonic: LD D, L
+    pub fn ld_d_l(&mut self) -> u8 {
+        self.registers.b = self.registers.l;
+        return 1;
+    }
+
+    /// OPCode: 0x56
+    /// Mnemonic: LD D, (HL)
+    pub fn ld_d_hlp(&mut self) -> u8 {
+        self.registers.b = self.read(self.registers.get_hl());
+        return 2;
+    }
+
+    /// OPCode: 0x57
+    /// Mnemonic: LD D, A
+    pub fn ld_d_a(&mut self) -> u8 {
+        self.registers.b = self.registers.a;
+        return 1;
+    }
+
+    /// OPCode: 0x58
+    /// Mnemonic: LD E, B
+    pub fn ld_e_b(&mut self) -> u8 {
+        self.registers.e = self.registers.b;
+        return 1;
+    }
+
+    /// OPCode: 0x59
+    /// Mnemonic: LD E, C
+    pub fn ld_e_c(&mut self) -> u8 {
+        self.registers.e = self.registers.c;
+        return 1;
+    }
+
+    /// OPCode: 0x5A
+    /// Mnemonic: LD E, D
+    pub fn ld_e_d(&mut self) -> u8 {
+        self.registers.e = self.registers.d;
+        return 1;
+    }
+
+    /// OPCode: 0x5B
+    /// Mnemonic: LD E, E
+    pub fn ld_e_e(&mut self) -> u8 {
+        self.registers.e = self.registers.e;
+        return 1;
+    }
+
+    /// OPCode: 0x5C
+    /// Mnemonic: LD E, H
+    pub fn ld_e_h(&mut self) -> u8 {
+        self.registers.e = self.registers.h;
+        return 1;
+    }
+
+    /// OPCode: 0x5D
+    /// Mnemonic: LD E, L
+    pub fn ld_e_l(&mut self) -> u8 {
+        self.registers.e = self.registers.l;
+        return 1;
+    }
+
+    /// OPCode: 0x5E
+    /// Mnemonic: LD E, (HL)
+    pub fn ld_e_hlp(&mut self) -> u8 {
+        self.registers.e = self.read(self.registers.get_hl());
+        return 2;
+    }
+
+    /// OPCode: 0x5F
+    /// Mnemonic: LD E, A
+    pub fn ld_e_a(&mut self) -> u8 {
+        self.registers.e = self.registers.a;
+        return 1;
+    }
 
     /// OPCode: 0xAF
     /// Mnemonic: XOR A
@@ -240,6 +485,229 @@ impl Cpu {
         return 1;
     }
 
+    /// OPCode: 0x60
+    /// Mnemonic: LD H, B
+    pub fn ld_h_b(&mut self) -> u8 {
+        self.registers.h = self.registers.b;
+        return 1;
+    }
+
+    /// OPCode: 0x61
+    /// Mnemonic: LD H, C
+    pub fn ld_h_c(&mut self) -> u8 {
+        self.registers.h = self.registers.c;
+        return 1;
+    }
+
+    /// OPCode: 0x62
+    /// Mnemonic: LD H, D
+    pub fn ld_h_d(&mut self) -> u8 {
+        self.registers.h = self.registers.d;
+        return 1;
+    }
+
+    /// OPCode: 0x63
+    /// Mnemonic: LD H, E
+    pub fn ld_h_e(&mut self) -> u8 {
+        self.registers.h = self.registers.e;
+        return 1;
+    }
+
+    /// OPCode: 0x64
+    /// Mnemonic: LD H, H
+    pub fn ld_h_h(&mut self) -> u8 {
+        self.registers.h = self.registers.h;
+        return 1;
+    }
+
+    /// OPCode: 0x65
+    /// Mnemonic: LD H, L
+    pub fn ld_h_l(&mut self) -> u8 {
+        self.registers.h = self.registers.l;
+        return 1;
+    }
+
+    /// OPCode: 0x66
+    /// Mnemonic: LD H, (HL)
+    pub fn ld_h_hlp(&mut self) -> u8 {
+        self.registers.h = self.read(self.registers.get_hl());
+        return 2;
+    }
+
+    /// OPCode: 0x67
+    /// Mnemonic: LD H, A
+    pub fn ld_h_a(&mut self) -> u8 {
+        self.registers.h = self.registers.a;
+        return 1;
+    }
+
+    /// OPCode: 0x68
+    /// Mnemonic: LD L, B
+    pub fn ld_l_b(&mut self) -> u8 {
+        self.registers.l = self.registers.b;
+        return 1;
+    }
+
+    /// OPCode: 0x69
+    /// Mnemonic: LD L, C
+    pub fn ld_l_c(&mut self) -> u8 {
+        self.registers.l = self.registers.c;
+        return 1;
+    }
+
+    /// OPCode: 0x6A
+    /// Mnemonic: LD L, D
+    pub fn ld_l_d(&mut self) -> u8 {
+        self.registers.l = self.registers.d;
+        return 1;
+    }
+
+    /// OPCode: 0x6B
+    /// Mnemonic: LD L, E
+    pub fn ld_l_e(&mut self) -> u8 {
+        self.registers.l = self.registers.e;
+        return 1;
+    }
+
+    /// OPCode: 0x6C
+    /// Mnemonic: LD L, H
+    pub fn ld_l_h(&mut self) -> u8 {
+        self.registers.l = self.registers.h;
+        return 1;
+    }
+
+    /// OPCode: 0x6D
+    /// Mnemonic: LD L, L
+    pub fn ld_l_l(&mut self) -> u8 {
+        self.registers.l = self.registers.l;
+        return 1;
+    }
+
+    /// OPCode: 0x6E
+    /// Mnemonic: LD L, (HL)
+    pub fn ld_l_hlp(&mut self) -> u8 {
+        self.registers.l = self.read(self.registers.get_hl());
+        return 2;
+    }
+
+    /// OPCode: 0x6F
+    /// Mnemonic: LD L, A
+    pub fn ld_l_a(&mut self) -> u8 {
+        self.registers.l = self.registers.a;
+        return 1;
+    }
+
+    /// OPCode: 0x70
+    /// Mnemonic: LD (HL), B
+    pub fn ld_hp_b(&mut self) -> u8 {
+        self.write_u8(self.registers.get_hl(), self.registers.b);
+        return 2;
+    }
+
+    /// OPCode: 0x71
+    /// Mnemonic: LD (HL), C
+    pub fn ld_hp_c(&mut self) -> u8 {
+        self.write_u8(self.registers.get_hl(), self.registers.c);
+        return 2;
+    }
+
+    /// OPCode: 0x72
+    /// Mnemonic: LD (HL), D
+    pub fn ld_hp_d(&mut self) -> u8 {
+        self.write_u8(self.registers.get_hl(), self.registers.d);
+        return 2;
+    }
+
+    /// OPCode: 0x73
+    /// Mnemonic: LD (HL), E
+    pub fn ld_hp_e(&mut self) -> u8 {
+        self.write_u8(self.registers.get_hl(), self.registers.e);
+        return 2;
+    }
+
+    /// OPCode: 0x74
+    /// Mnemonic: LD (HL), H
+    pub fn ld_hp_h(&mut self) -> u8 {
+        self.write_u8(self.registers.get_hl(), self.registers.h);
+        return 2;
+    }
+
+    /// OPCode: 0x75
+    /// Mnemonic: LD (HL), L
+    pub fn ld_hp_l(&mut self) -> u8 {
+        self.write_u8(self.registers.get_hl(), self.registers.l);
+        return 2;
+    }
+
+    /// OPCode: 0x76
+    /// Mnemonic: HALT
+    pub fn ld_hp_hlp(&mut self) -> u8 {
+        println!("HALT not implemented!");
+        return 0;
+    }
+
+    /// OPCode: 0x77
+    /// Mnemonic: LD (HL), A
+    pub fn halt(&mut self) -> u8 {
+        self.write_u8(self.registers.get_hl(), self.registers.a);
+        return 2;
+    }
+
+    /// OPCode: 0x78
+    /// Mnemonic: LD A, B
+    pub fn ld_a_b(&mut self) -> u8 {
+        self.registers.a = self.registers.b;
+        return 1;
+    }
+
+    /// OPCode: 0x79
+    /// Mnemonic: LD A, C
+    pub fn ld_a_c(&mut self) -> u8 {
+        self.registers.a = self.registers.c;
+        return 1;
+    }
+
+    /// OPCode: 0x7A
+    /// Mnemonic: LD A, D
+    pub fn ld_a_d(&mut self) -> u8 {
+        self.registers.a = self.registers.d;
+        return 1;
+    }
+
+    /// OPCode: 0x7B
+    /// Mnemonic: LD A, E
+    pub fn ld_a_e(&mut self) -> u8 {
+        self.registers.a = self.registers.e;
+        return 1;
+    }
+
+    /// OPCode: 0x7C
+    /// Mnemonic: LD A, H
+    pub fn ld_a_h(&mut self) -> u8 {
+        self.registers.a = self.registers.h;
+        return 1;
+    }
+
+    /// OPCode: 0x7D
+    /// Mnemonic: LD A, L
+    pub fn ld_a_l(&mut self) -> u8 {
+        self.registers.a = self.registers.l;
+        return 1;
+    }
+
+    /// OPCode: 0x7E
+    /// Mnemonic: LD A, (HL)
+    pub fn ld_a_hlp(&mut self) -> u8 {
+        self.registers.a = self.read(self.registers.get_hl());
+        return 2;
+    }
+
+    /// OPCode: 0x7F
+    /// Mnemonic: LD A, A
+    pub fn ld_a_a(&mut self) -> u8 {
+        self.registers.a = self.registers.a;
+        return 1;
+    }
     /// OPCode: 0xC3
     /// Mnemonic: JP
     pub fn jp_a16(&mut self) -> u8 {
@@ -255,7 +723,7 @@ impl Cpu {
         return 4;
     }
 
-    // 0xCB 
+    // 0xCB extended set:
 
     /// 0xCBFE
     /// Mnemonic: SET 7, (HL)
