8a708298406c82c283f0da2373679f90069b3218
Marius Zander <marius@bam-bam-bam.com>
2023-08-13 02:41:24 +0200

    add sub hc check

--- src/cpu/operations.rs
+++ src/cpu/operations.rs
@@ -148,6 +148,14 @@ impl Cpu {
     fn is_add_u16_hc(left: u16, right: u16) -> bool {
         ((left & 0xff) + (right & 0xff)) & 0x100 == 0x100
     }
+
+    fn is_sub_u8_hc(left: u8, right: u8) -> bool {
+        ((left & 0xf) - (right & 0xf)) & 0x10 == 0x10
+    }
+
+    fn is_sub_u16_hc(left: u16, right: u16) -> bool {
+        ((left & 0xff) - (right & 0xff)) & 0x100 == 0x100
+    }
 }
 
 #[cfg(test)]
