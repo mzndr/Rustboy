a047e25449830d9ca4c2df3f68f78fa1391e6c54
Marius Zander <marius@bam-bam-bam.com>
2023-10-28 18:51:30 +0200

    fix: use initial values for registers
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -26,16 +26,16 @@ pub struct Registers {
 impl Registers {
     pub fn new() -> Registers {
         Registers {
-            a: 0x00,
-            f: 0x00,
+            a: 0x01,
+            f: 0xb0,
             b: 0x00,
-            c: 0x00,
+            c: 0x13,
             d: 0x00,
-            e: 0x00,
-            h: 0x00,
-            l: 0x00,
-            sp: 0x000,
-            pc: 0x100,
+            e: 0xd8,
+            h: 0x01,
+            l: 0x4d,
+            sp: 0xfffe,
+            pc: 0x0100,
 
             ime: false,
         }
