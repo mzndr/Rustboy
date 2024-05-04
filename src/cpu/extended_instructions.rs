438ce5495c5706282cab5a93ebeb703b34b748ce
Marius Zander <marius@bam-bam-bam.com>
2024-05-04 12:04:36 +0200

    fix: dereference read value

--- src/cpu/extended_instructions.rs
+++ src/cpu/extended_instructions.rs
@@ -13,7 +13,7 @@ impl Cpu {
     /// Mnemonic: `SET 7, (HL)`
     pub fn set_7_hlp(&mut self) -> u8 {
         let hl = self.registers.get_hl();
-        let val = self.read(hl);
+        let val = *self.read(hl);
         let res = Self::set_nth_bit(7, val);
         self.write_u8(hl, res);
         3
