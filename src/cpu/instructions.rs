037e994e7fcea5c8fa0fb25dcd5db0b7a5c3a073
Marius Zander <marius@bam-bam-bam.com>
2024-05-04 14:24:09 +0200

    refactor: implement arithmetic functions by decoding register accesses where possible

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -140,93 +140,109 @@ impl Cpu {
             }
             0x40..=0x7f => Self::ld(self.registers[src_idx], &mut self.registers[dst_idx]),
 
-            0x80 => self.add_a_b(),
-            0x81 => self.add_a_c(),
-            0x82 => self.add_a_d(),
-            0x83 => self.add_a_e(),
-            0x84 => self.add_a_h(),
-            0x85 => self.add_a_l(),
-            0x86 => self.add_a_hlp(),
-            0x87 => self.add_a_a(),
-            0x88 => self.adc_a_b(),
-            0x89 => self.adc_a_c(),
-            0x8a => self.adc_a_d(),
-            0x8b => self.adc_a_e(),
-            0x8c => self.adc_a_h(),
-            0x8d => self.adc_a_l(),
-            0x8e => self.adc_a_hlp(),
-            0x8f => self.adc_a_a(),
-            0x90 => self.sub_a_b(),
-            0x91 => self.sub_a_c(),
-            0x92 => self.sub_a_d(),
-            0x93 => self.sub_a_e(),
-            0x94 => self.sub_a_h(),
-            0x95 => self.sub_a_l(),
-            0x96 => self.sub_a_hlp(),
-            0x97 => self.sub_a_a(),
-            0x98 => self.sbc_a_b(),
-            0x99 => self.sbc_a_c(),
-            0x9a => self.sbc_a_d(),
-            0x9b => self.sbc_a_e(),
-            0x9c => self.sbc_a_h(),
-            0x9d => self.sbc_a_l(),
-            0x9e => self.sbc_a_hlp(),
-            0x9f => self.sbc_a_a(),
-            0xC6 => self.add_a_u8(),
-            0xD6 => self.sub_a_u8(),
-            0xE6 => self.and_u8(),
-            0xF6 => self.or_u8(),
-            0xa0 => self.and_b(),
-            0xa1 => self.and_c(),
-            0xa2 => self.and_d(),
-            0xa3 => self.and_e(),
-            0xa4 => self.and_h(),
-            0xa5 => self.and_l(),
-            0xa6 => self.and_hlp(),
-            0xa7 => self.and_a(),
-            0xa8 => self.xor_b(),
-            0xa9 => self.xor_c(),
-            0xaa => self.xor_d(),
-            0xab => self.xor_e(),
-            0xac => self.xor_h(),
-            0xad => self.xor_l(),
-            0xae => self.xor_hlp(),
-            0xaf => self.xor_a(),
-            0xb0 => self.or_b(),
-            0xb1 => self.or_c(),
-            0xb2 => self.or_d(),
-            0xb3 => self.or_e(),
-            0xb4 => self.or_h(),
-            0xb5 => self.or_l(),
-            0xb6 => self.or_hlp(),
-            0xb7 => self.or_a(),
-            0xb8 => self.cp_b(),
-            0xb9 => self.cp_c(),
-            0xba => self.cp_d(),
-            0xbb => self.cp_e(),
-            0xbc => self.cp_h(),
-            0xbd => self.cp_l(),
-            0xbe => self.cp_hlp(),
-            0xbf => self.cp_a(),
+            0x86 => {
+                self.add8(*self.read(self.registers.get_hl()));
+                2
+            }
+            0x80..=0x87 => self.add8(self.registers[src_idx]),
+            0x8E => {
+                self.add8c(*self.read(self.registers.get_hl()));
+                2
+            }
+            0x88..=0x8F => self.add8c(self.registers[src_idx]),
+
+
+            0x96 => {
+                self.sub8(*self.read(self.registers.get_hl()));
+                2
+            }
+            0x90..=0x97 => self.sub8(self.registers[src_idx]),
+            0x9E => {
+                self.sub8c(*self.read(self.registers.get_hl()));
+                2
+            }
+            0x98..=0x9F => self.sub8c(self.registers[src_idx]),
+
+            0xa6 => {
+                self.and(*self.read(self.registers.get_hl()));
+                2
+            }
+            0xa0..=0xa7 => self.and(self.registers[src_idx]),
+            0xae => {
+                self.xor(*self.read(self.registers.get_hl()));
+                2
+            }
+            0xa8..=0xaf => self.xor(self.registers[src_idx]),
+
+            0xb6 => {
+                self.or(*self.read(self.registers.get_hl()));
+                2
+            }
+            0xb0..=0xb7 => self.or(self.registers[src_idx]),
+            0xbe => {
+                self.cp(*self.read(self.registers.get_hl()));
+                2
+            }
+            0xb8..=0xbf => self.cp(self.registers[src_idx]),
+
+            0xc6 => {
+                let d8 = self.read_u8_at_pc_and_increase();
+                self.add8(d8);
+                2
+            },
+            0xd6 => {
+                let d8 = self.read_u8_at_pc_and_increase();
+                self.sub8(d8);
+                2
+            },
+            0xe6 => {
+                let d8 = self.read_u8_at_pc_and_increase();
+                self.and(d8);
+                2
+            },
+            0xf6 => {
+                let d8 = self.read_u8_at_pc_and_increase();
+                self.or(d8);
+                2
+            },
+
+
+            0xce => {
+                let d8 = self.read_u8_at_pc_and_increase();
+                self.add8c(d8);
+                2
+            },
+            0xde => {
+                let d8 = self.read_u8_at_pc_and_increase();
+                self.sub8c(d8);
+                2
+            },
+            0xee => {
+                let d8 = self.read_u8_at_pc_and_increase();
+                self.xor(d8);
+                2
+            },
+            0xfe => {
+                let d8 = self.read_u8_at_pc_and_increase();
+                self.cp(d8);
+                2
+            },
+
             0xc1 => self.pop_bc(),
             0xc3 => self.jp_a16(),
             0xc5 => self.push_bc(),
             0xcb => self.exec_cb_instruction(),
-            0xce => self.adc_a_u8(),
             0xcf => self.rst_08h(),
             0xd1 => self.pop_de(),
             0xd5 => self.push_de(),
-            0xde => self.sbc_a_u8(),
             0xdf => self.rst_18h(),
             0xe1 => self.pop_hl(),
             0xe5 => self.push_hl(),
-            0xee => self.xor_u8(),
             0xef => self.rst_28h(),
             0xf1 => self.pop_af(),
             0xf3 => self.di(),
             0xf5 => self.push_af(),
             0xfa => self.ei(),
-            0xfe => self.cp_u8(),
             0xff => self.rst_38h(),
             _ => panic!("Unknown instruction: 0x{opcode:x}"),
         }
@@ -538,523 +554,6 @@ impl Cpu {
         let r = self.registers.get_sp();
         let res = Self::inc16(r);
         self.registers.set_sp(res);
-        2
-    }
-
-    /// OP-Code: `0xA8`
-    /// Mnemonic: `AND B`
-    pub fn and_b(&mut self) -> u8 {
-        self.and(self.registers.b);
-        1
-    }
-
-    /// OP-Code: `0xA9`
-    /// Mnemonic: `AND C`
-    pub fn and_c(&mut self) -> u8 {
-        self.and(self.registers.c);
-        1
-    }
-
-    /// OP-Code: `0xAA`
-    /// Mnemonic: `AND D`
-    pub fn and_d(&mut self) -> u8 {
-        self.and(self.registers.d);
-        1
-    }
-
-    /// OP-Code: `0xAB`
-    /// Mnemonic: `AND E`
-    pub fn and_e(&mut self) -> u8 {
-        self.and(self.registers.e);
-        1
-    }
-
-    /// OP-Code: `0xAC`
-    /// Mnemonic: `AND H`
-    pub fn and_h(&mut self) -> u8 {
-        self.and(self.registers.h);
-        1
-    }
-
-    /// OP-Code: `0xAD`
-    /// Mnemonic: `AND L`
-    pub fn and_l(&mut self) -> u8 {
-        self.and(self.registers.l);
-        1
-    }
-
-    /// OP-Code: `0xAE`
-    /// Mnemonic: `AND (HL)`
-    pub fn and_hlp(&mut self) -> u8 {
-        self.and(*self.read(self.registers.get_hl()));
-        2
-    }
-
-    /// OP-Code: `0xAF`
-    /// Mnemonic: `AND A`
-    pub fn and_a(&mut self) -> u8 {
-        self.and(self.registers.a);
-        1
-    }
-
-    /// OP-Code: `0xE6`
-    /// Mnemonic: `AND u8`
-    pub fn and_u8(&mut self) -> u8 {
-        let val = self.read_u8_at_pc_and_increase();
-        self.and(val);
-        2
-    }
-
-    /// OP-Code: `0xA8`
-    /// Mnemonic: `XOR B`
-    pub fn xor_b(&mut self) -> u8 {
-        self.xor(self.registers.b);
-        1
-    }
-
-    /// OP-Code: `0xA9`
-    /// Mnemonic: `XOR C`
-    pub fn xor_c(&mut self) -> u8 {
-        self.xor(self.registers.c);
-        1
-    }
-
-    /// OP-Code: `0xAA`
-    /// Mnemonic: `XOR D`
-    pub fn xor_d(&mut self) -> u8 {
-        self.xor(self.registers.d);
-        1
-    }
-
-    /// OP-Code: `0xAB`
-    /// Mnemonic: `XOR E`
-    pub fn xor_e(&mut self) -> u8 {
-        self.xor(self.registers.e);
-        1
-    }
-
-    /// OP-Code: `0xAC`
-    /// Mnemonic: `XOR H`
-    pub fn xor_h(&mut self) -> u8 {
-        self.xor(self.registers.h);
-        1
-    }
-
-    /// OP-Code: `0xAD`
-    /// Mnemonic: `XOR L`
-    pub fn xor_l(&mut self) -> u8 {
-        self.xor(self.registers.l);
-        1
-    }
-
-    /// OP-Code: `0xAE`
-    /// Mnemonic: `XOR (HL)`
-    pub fn xor_hlp(&mut self) -> u8 {
-        self.xor(*self.read(self.registers.get_hl()));
-        2
-    }
-
-    /// OP-Code: `0xAF`
-    /// Mnemonic: `XOR A`
-    pub fn xor_a(&mut self) -> u8 {
-        self.xor(self.registers.a);
-        1
-    }
-
-    /// OP-Code: `0xEE`
-    /// Mnemonic: `XOR u8`
-    pub fn xor_u8(&mut self) -> u8 {
-        let val = self.read_u8_at_pc_and_increase();
-        self.xor(val);
-        2
-    }
-
-    /// OP-Code: `0xB0`
-    /// Mnemonic: `OR B`
-    pub fn or_b(&mut self) -> u8 {
-        let val = self.registers.b;
-        self.or(val);
-        1
-    }
-
-    /// OP-Code: `0xB1`
-    /// Mnemonic: `OR C`
-    pub fn or_c(&mut self) -> u8 {
-        self.or(self.registers.c);
-        1
-    }
-
-    /// OP-Code: `0xB2`
-    /// Mnemonic: `OR D`
-    pub fn or_d(&mut self) -> u8 {
-        self.or(self.registers.d);
-        1
-    }
-
-    /// OP-Code: `0xB3`
-    /// Mnemonic: `OR E`
-    pub fn or_e(&mut self) -> u8 {
-        self.or(self.registers.e);
-        1
-    }
-
-    /// OP-Code: `0xB4`
-    /// Mnemonic: `OR H`
-    pub fn or_h(&mut self) -> u8 {
-        self.or(self.registers.h);
-        1
-    }
-
-    /// OP-Code: `0xB5`
-    /// Mnemonic: `OR L`
-    pub fn or_l(&mut self) -> u8 {
-        self.or(self.registers.l);
-        1
-    }
-
-    /// OP-Code: `0xB6`
-    /// Mnemonic: `OR (HL)`
-    pub fn or_hlp(&mut self) -> u8 {
-        self.or(*self.read(self.registers.get_hl()));
-        2
-    }
-
-    /// OP-Code: `0xB7`
-    /// Mnemonic: `OR A`
-    pub fn or_a(&mut self) -> u8 {
-        self.or(self.registers.a);
-        1
-    }
-
-    /// OP-Code: `0xF6`
-    /// Mnemonic: `OR u8`
-    pub fn or_u8(&mut self) -> u8 {
-        let val = self.read_u8_at_pc_and_increase();
-        self.or(val);
-        2
-    }
-
-    /// OP-Code: `0xB8`
-    /// Mnemonic: `CP B`
-    pub fn cp_b(&mut self) -> u8 {
-        self.cp(self.registers.b);
-        1
-    }
-
-    /// OP-Code: `0xB9`
-    /// Mnemonic: `CP C`
-    pub fn cp_c(&mut self) -> u8 {
-        self.cp(self.registers.c);
-        1
-    }
-
-    /// OP-Code: `0xBA`
-    /// Mnemonic: `CP D`
-    pub fn cp_d(&mut self) -> u8 {
-        self.cp(self.registers.d);
-        1
-    }
-
-    /// OP-Code: `0xBB`
-    /// Mnemonic: `CP E`
-    pub fn cp_e(&mut self) -> u8 {
-        self.cp(self.registers.e);
-        1
-    }
-
-    /// OP-Code: `0xBC`
-    /// Mnemonic: `CP H`
-    pub fn cp_h(&mut self) -> u8 {
-        self.cp(self.registers.h);
-        1
-    }
-
-    /// OP-Code: `0xBD`
-    /// Mnemonic: `CP L`
-    pub fn cp_l(&mut self) -> u8 {
-        self.cp(self.registers.l);
-        1
-    }
-
-    /// OP-Code: `0xBE`
-    /// Mnemonic: `CP (HL)`
-    pub fn cp_hlp(&mut self) -> u8 {
-        let address = self.registers.get_hl();
-        self.cp(*self.read(address));
-        2
-    }
-
-    /// OP-Code: `0xBF`
-    /// Mnemonic: `CP A`
-    pub fn cp_a(&mut self) -> u8 {
-        self.cp(self.registers.a);
-        1
-    }
-
-    /// OP-Code: `0xFE`
-    /// Mnemonic: `CP u8`
-    pub fn cp_u8(&mut self) -> u8 {
-        let val = self.read_u8_at_pc_and_increase();
-        self.cp(val);
-        2
-    }
-
-    /// OP-Code: `0x80`
-    /// Mnemonic: `ADD A B`
-    pub fn add_a_b(&mut self) -> u8 {
-        self.add8(self.registers.b);
-        1
-    }
-
-    /// OP-Code: `0x81`
-    /// Mnemonic: `ADD A C`
-    pub fn add_a_c(&mut self) -> u8 {
-        self.add8(self.registers.c);
-        1
-    }
-
-    /// OP-Code: `0x82`
-    /// Mnemonic: `ADD A D`
-    pub fn add_a_d(&mut self) -> u8 {
-        self.add8(self.registers.d);
-        1
-    }
-
-    /// OP-Code: `0x83`
-    /// Mnemonic: `ADD A E`
-    pub fn add_a_e(&mut self) -> u8 {
-        self.add8(self.registers.e);
-        1
-    }
-
-    /// OP-Code: `0x84`
-    /// Mnemonic: `ADD A H`
-    pub fn add_a_h(&mut self) -> u8 {
-        self.add8(self.registers.h);
-        1
-    }
-
-    /// OP-Code: `0x85`
-    /// Mnemonic: `ADD A L`
-    pub fn add_a_l(&mut self) -> u8 {
-        self.add8(self.registers.l);
-        1
-    }
-
-    /// OP-Code: `0x86`
-    /// Mnemonic: `ADD A (HL)`
-    pub fn add_a_hlp(&mut self) -> u8 {
-        let address = self.registers.get_hl();
-        self.add8(*self.read(address));
-        2
-    }
-
-    /// OP-Code: `0x87`
-    /// Mnemonic: `ADD A A`
-    pub fn add_a_a(&mut self) -> u8 {
-        self.add8(self.registers.a);
-        1
-    }
-
-    /// OP-Code: `0xC6`
-    /// Mnemonic: `ADD A A`
-    pub fn add_a_u8(&mut self) -> u8 {
-        self.add8(self.registers.a);
-        2
-    }
-
-    /// OP-Code: `0x88`
-    /// Mnemonic: `ADC A B`
-    pub fn adc_a_b(&mut self) -> u8 {
-        self.add8c(self.registers.b);
-        1
-    }
-
-    /// OP-Code: `0x89`
-    /// Mnemonic: `ADC A C`
-    pub fn adc_a_c(&mut self) -> u8 {
-        self.add8c(self.registers.c);
-        1
-    }
-
-    /// OP-Code: `0x8A`
-    /// Mnemonic: `ADC A D`
-    pub fn adc_a_d(&mut self) -> u8 {
-        self.add8c(self.registers.d);
-        1
-    }
-
-    /// OP-Code: `0x8B`
-    /// Mnemonic: `ADC A E`
-    pub fn adc_a_e(&mut self) -> u8 {
-        self.add8c(self.registers.e);
-        1
-    }
-
-    /// OP-Code: `0x8C`
-    /// Mnemonic: `ADC A H`
-    pub fn adc_a_h(&mut self) -> u8 {
-        self.add8c(self.registers.h);
-        1
-    }
-
-    /// OP-Code: `0x8D`
-    /// Mnemonic: `ADC A L`
-    pub fn adc_a_l(&mut self) -> u8 {
-        self.add8c(self.registers.l);
-        1
-    }
-
-    /// OP-Code: `0x8E`
-    /// Mnemonic: `ADC A L`
-    pub fn adc_a_hlp(&mut self) -> u8 {
-        let address = self.registers.get_hl();
-        self.add8c(*self.read(address));
-        2
-    }
-
-    /// OP-Code: `0x8F`
-    /// Mnemonic: `ADC A A`
-    pub fn adc_a_a(&mut self) -> u8 {
-        self.add8c(self.registers.a);
-        1
-    }
-
-    /// OP-Code: `0xce`
-    /// Mnemonic: `ADC A u8`
-    pub fn adc_a_u8(&mut self) -> u8 {
-        let val = self.read_u8_at_pc_and_increase();
-        self.add8c(val);
-        2
-    }
-
-    /// OP-Code: `0x90`
-    /// Mnemonic: `SUB A B`
-    pub fn sub_a_b(&mut self) -> u8 {
-        self.sub8(self.registers.b);
-        1
-    }
-
-    /// OP-Code: `0x91`
-    /// Mnemonic: `SUB A C`
-    pub fn sub_a_c(&mut self) -> u8 {
-        self.sub8(self.registers.c);
-        1
-    }
-
-    /// OP-Code: `0x92`
-    /// Mnemonic: `SUB A D`
-    pub fn sub_a_d(&mut self) -> u8 {
-        self.sub8(self.registers.d);
-        1
-    }
-
-    /// OP-Code: `0x93`
-    /// Mnemonic: `SUB A E`
-    pub fn sub_a_e(&mut self) -> u8 {
-        self.sub8(self.registers.e);
-        1
-    }
-
-    /// OP-Code: `0x94`
-    /// Mnemonic: `SUB A H`
-    pub fn sub_a_h(&mut self) -> u8 {
-        self.sub8(self.registers.h);
-        1
-    }
-
-    /// OP-Code: `0x95`
-    /// Mnemonic: `SUB A L`
-    pub fn sub_a_l(&mut self) -> u8 {
-        self.sub8(self.registers.l);
-        1
-    }
-
-    /// OP-Code: `0x96`
-    /// Mnemonic: `SUB A (HL)`
-    pub fn sub_a_hlp(&mut self) -> u8 {
-        let address = self.registers.get_hl();
-        self.sub8(*self.read(address));
-        2
-    }
-
-    /// OP-Code: `0x97`
-    /// Mnemonic: `SUB A A`
-    pub fn sub_a_a(&mut self) -> u8 {
-        self.sub8(self.registers.a);
-        1
-    }
-
-    /// OP-Code: `0xD6`
-    /// Mnemonic: `SUB A u8`
-    pub fn sub_a_u8(&mut self) -> u8 {
-        let val = self.read_u8_at_pc_and_increase();
-        self.sub8(val);
-        1
-    }
-
-    /// OP-Code: `0x98`
-    /// Mnemonic: `SBC A B`
-    pub fn sbc_a_b(&mut self) -> u8 {
-        self.sub8c(self.registers.b);
-        1
-    }
-
-    /// OP-Code: `0x99`
-    /// Mnemonic: `SBC A C`
-    pub fn sbc_a_c(&mut self) -> u8 {
-        self.sub8c(self.registers.c);
-        1
-    }
-
-    /// OP-Code: `0x9A`
-    /// Mnemonic: `SBC A D`
-    pub fn sbc_a_d(&mut self) -> u8 {
-        self.sub8c(self.registers.d);
-        1
-    }
-
-    /// OP-Code: `0x9B`
-    /// Mnemonic: `SBC A E`
-    pub fn sbc_a_e(&mut self) -> u8 {
-        self.sub8c(self.registers.e);
-        1
-    }
-
-    /// OP-Code: `0x9C`
-    /// Mnemonic: `SBC A H`
-    pub fn sbc_a_h(&mut self) -> u8 {
-        self.sub8c(self.registers.h);
-        1
-    }
-
-    /// OP-Code: `0x9D`
-    /// Mnemonic: `SBC A L`
-    pub fn sbc_a_l(&mut self) -> u8 {
-        self.sub8c(self.registers.l);
-        1
-    }
-
-    /// OP-Code: `0x9E`
-    /// Mnemonic: `SBC A L`
-    pub fn sbc_a_hlp(&mut self) -> u8 {
-        let address = self.registers.get_hl();
-        self.sub8c(*self.read(address));
-        2
-    }
-
-    /// OP-Code: `0x9F`
-    /// Mnemonic: `SBC A A`
-    pub fn sbc_a_a(&mut self) -> u8 {
-        self.sub8c(self.registers.a);
-        1
-    }
-
-    /// OP-Code: `0xDE`
-    /// Mnemonic: `SBC A u8`
-    pub fn sbc_a_u8(&mut self) -> u8 {
-        let val = self.read_u8_at_pc_and_increase();
-        self.sub8c(val);
         2
     }
 
