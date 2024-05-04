c18f4596ef445f41918e427d16900e071c6978a3
Marius Zander <marius@bam-bam-bam.com>
2024-05-05 00:10:52 +0200

    docs: add todo

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -11,6 +11,8 @@ const FLAG_C_INDEX: u8 = 0x04;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Registers {
+    /// Maybe swap these out for an actual array to do better 
+    /// combined register access.
     pub a: u8,
     pub f: u8,
     pub b: u8,
