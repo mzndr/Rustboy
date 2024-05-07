eaad3deb7dc1005f5885831460477a92f2b6c999
Marius Zander <marius@bam-bam-bam.com>
2024-05-07 23:47:43 +0200

    feat: implement first few extended instructions
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- src/cpu/extended_instructions.rs
+++ src/cpu/extended_instructions.rs
@@ -3,19 +3,25 @@ use super::Cpu;
 impl Cpu {
     pub fn exec_cb_instruction(&mut self) -> u8 {
         let opcode = self.read_u8_at_pc_and_increase();
+        let dst_idx = opcode & 0b1111_0000 >> 4;
+        let src_idx = opcode & 0b1111;
         match opcode {
-            0xFE => self.set_7_hlp(),
+            0x06 => self.rlc_hl(),
+            0x0e => self.rrc_hl(),
+
+            0x00..=0x07 => self.rlc(dst_idx),
+            0x08..=0x0F => self.rrc(dst_idx),
+            0x10..=0x17 => self.rl(dst_idx),
+            0x18..=0x1F => self.rr(dst_idx),
             _ => panic!("Unknown extended instruction: 0xCB{opcode:x}"),
         }
     }
 
-    /// OP-Code: `0xCBFE`
-    /// Mnemonic: `SET 7, (HL)`
-    pub fn set_7_hlp(&mut self) -> u8 {
-        let hl = self.registers.get_hl();
-        let val = *self.read(hl);
-        let res = Self::set_nth_bit(7, val);
-        self.write_u8(hl, res);
-        3
+    fn rrc_hl(&mut self) -> u8 {
+        todo!("rrc (hl)")
+    }
+
+    fn rlc_hl(&mut self) -> u8 {
+        todo!("rlc (hl)")
     }
 }
