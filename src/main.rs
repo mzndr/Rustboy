c8cc045b7deb1c4ce49684bd82029897a2f7eb30
Marius Zander <marius@bam-bam-bam.com>
2024-05-18 01:57:54 +0200

    feat: add gameboy struct
    Signed-off-by: Marius Zander <hello@mzndr.me>

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,17 @@
 #![warn(clippy::pedantic, clippy::unwrap_used)]
 
 use std::{fs, path};
-use std::{thread, time};
 
 use clap::Parser;
-use time::Duration;
 use tracing_subscriber::EnvFilter;
 
 use crate::cpu::disassembler::disassemble_rom;
-use crate::cpu::Cpu;
+use crate::gb::Gameboy;
 
+mod apu;
 mod cpu;
-
-const CLOCK_SPEED: f32 = 4100f32;
+mod gb;
+mod ppu;
 
 /// Command line arguments, parsed by [`clap`].
 #[derive(Parser, Debug)]
@@ -33,7 +32,6 @@ fn main() {
     let args = Args::parse();
     tracing::info!(?args, "starting emulator");
 
-    let mut cpu = Cpu::new();
     let rom = fs::read(path::Path::new(&args.rom_path)).expect("cannot read ROM");
 
     if args.disassemble {
@@ -42,44 +40,7 @@ fn main() {
         return;
     }
 
-    cpu.load_rom(&rom);
-    clock_loop(&mut cpu);
-}
-
-/// Executed in loop at roughly 4mhz.
-fn clock_cycle() {}
-
-/// Executed every 4 clock clycles or roughly 1mhz.
-fn machine_cycle(cpu: &mut Cpu) {
-    cpu.cycle();
-}
-
-fn clock_loop(cpu: &mut Cpu) {
-    let mut cycles: u8 = 1;
-    loop {
-        let start = time::Instant::now();
-        clock_cycle();
-        if cycles == 4 {
-            cycles = 0;
-            machine_cycle(cpu);
-        }
-        sleep_till_next_cycle(start);
-        cycles += 1;
-    }
-}
-
-//TODO: Actually properly convert values
-#[allow(
-    clippy::cast_precision_loss,
-    clippy::cast_sign_loss,
-    clippy::cast_possible_truncation
-)]
-fn sleep_till_next_cycle(start: time::Instant) {
-    let cycles_per_ms: f32 = CLOCK_SPEED / 1000.0; // 1mhz
-    let after = time::Instant::now();
-    let passed = after - start;
-    let passed_nano = passed.as_nanos();
-    let delta = Duration::from_nanos(((1000.0 / cycles_per_ms) - passed_nano as f32) as u64);
-
-    thread::sleep(delta);
+    let mut gb = Gameboy::new();
+    gb.load_rom(&rom);
+    gb.run();
 }
